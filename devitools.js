4dc3d31914ba2c4e530e2fdcb88e6a13214313c6
William Correa <wilcorrea@gmail.com>
2021-06-07 05:43:30 -0300

    (chore) Improve dev configuration

--- devitools.js
+++ devitools.js
@@ -1,9 +1,58 @@
+import Path from 'path'
+
+
 /**
  * @param command
- * @param git
+ * @param {string} pwd
+ * @param {string} name
+ * @param {string} short
+ * @private
+ */
+function update(command, pwd, name, short) {
+  const files = [
+    '.environment/stage/docker-compose.yml',
+    '.tevun/hooks/install.sh',
+    '.tevun/hooks/setup.sh',
+
+    'frontend/public/statics/site.webmanifest',
+    'frontend/.env.defaults',
+    'frontend/.env.example',
+    'frontend/package.json',
+    'frontend/quasar.conf.js',
+
+    'backend/.env.defaults',
+    'backend/.env.example',
+    'backend/composer.json',
+    'backend/docker-compose.yml.example',
+    'backend/makefile',
+
+    '.devitools.json',
+  ]
+  for (const file of files) {
+    const filename = Path.resolve(pwd, file)
+    const string = String(command.readFile(filename))
+    const content = string
+      .replace(/replace\.app\.name/g, name)
+      .replace(/replace\.app\.short/g, short)
+    command.writeFile(filename, content)
+  }
+}
+
+/**
+ * @param command
+ * @param pwd
+ * @param options
  * @return {Promise<void>}
  */
-module.exports = async function(command, git) {
+module.exports = async function(command, pwd, options) {
+  command.unlink(Path.resolve(pwd, '.git'))
+  command.unlink(Path.resolve(pwd, 'frontend/@devitools'))
+  command.unlink(Path.resolve(pwd, 'backend/@devitools'))
+
+  const { name, short, git } = options
+
+  update(command, pwd, name, short)
+
   await git
     .init()
     .checkoutLocalBranch('main')
